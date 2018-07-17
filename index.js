80f2cad5293b126c1d954a6164c0d03c75e7ea13
Ben <menubar@protonmail.com>
2018-07-17 01:23:44 -0400

    new

---
+++ index.js
@@ -0,0 +1,35 @@
+const werist = require('node-werist')
+
+const whoisResolve = (name) => {
+  return new Promise((resolve, reject) => {
+    werist.lookup(name, function(err, data) {
+      let line = data.split(/\r\n/g)[0].trim()
+
+      if (/^[Nn][Oo][Tt]?\s/.test(line)) {
+        resolve(false)
+      }
+      resolve(true)
+    })
+  })
+}
+
+const nativeDnsResolve = (name) => {
+  return new Promise((resolve, reject) => {
+    require('dns').resolve(name, (err, data) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(false)
+    })
+  })
+}
+
+module.exports = async (name) => {
+  let available
+  try {
+    available = await nativeDnsResolve(name)
+  } catch (err) {
+    available = await whoisResolve(name)
+  }
+  return available
+}
